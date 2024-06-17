ef6f4852c477ee42663f33921d26d095a7c66df2
shaeelhashmi <shaeelh001@gmail.com>
2024-06-17 04:00:13 -0700

    Fixed issues with form

--- src/Components/InputTags.tsx
+++ src/Components/InputTags.tsx
@@ -5,19 +5,22 @@ interface InputTagsProps {
   label: string;
   type: string;
   placeholder: string;
-  id?: string; // id is optional
 }
 
 export default function InputTags(props: InputTagsProps) {
+  const handleChange = (e:React.FormEvent<HTMLInputElement>) => {
+   e.currentTarget.value = e.currentTarget.value.toLowerCase();
+   e.currentTarget.value = e.currentTarget.value.split(" ").join("");
+  }
   return (
     <div className="flex items-center mt-8">
       <label htmlFor={props.name} className='w-1/3 text-lg'>{props.label}</label>
       <input 
         type={props.type} 
         name={props.name} 
-        id={props.id} 
         placeholder={props.placeholder} 
         className='w-[63%] p-3 text-sm bg-[#171617] border-l-0 border-r-0 border-t-0 border-b'
+        onChange={props.name === 'username' ? handleChange : (e:React.FormEvent<HTMLInputElement>) => e.currentTarget.value}
       />
     </div>
   );
--- src/Components/Registration.tsx
+++ src/Components/Registration.tsx
@@ -1,18 +1,47 @@
 
 import { Link } from 'react-router-dom';
 import InputTags from './InputTags.tsx';
+import React,{ FormEvent,useState } from 'react'
 export default function Registration() {
+  const [error,setError]=useState('')
+  const IsEmpty=(username:String,password:String):boolean=>{
+    if(username.length===0 || password.length===0){
+      return true
+    }
+    return false
+  } 
+  const IsEmptySpaces=(username:String):boolean=>{
+    if(username.trim().length===0){
+      return true
+    }
+    return false;
+  }
+  const Submit:React.FormEventHandler<HTMLFormElement> = (e:FormEvent<HTMLFormElement>) => {
+    const Form=e.currentTarget;
+    let username = (Form.elements.namedItem('username') as HTMLInputElement).value;
+    let password = (Form.elements.namedItem('password') as HTMLInputElement).value;
+    e.preventDefault();
+    if(IsEmpty(username,password)){
+      setError('Please fill all the fields')
+      return;
+    }
+    if(IsEmptySpaces(username)){
+      setError('Username cannot be empty spaces')
+      return;
+    }
+  }
 
   return (
     <>
     <div className='flex items-center justify-center min-h-screen'>
     <div className='w-[400px] h-[360px] p-3  rounded-3xl bg-opacity-100 text-white border-[#0C0B0C] border-solid border-4'>
       <h1 className='text-3xl text-center '>Sign Up</h1> 
-      <form action="/register" method="post">
+      <form action="/register" method="post" onSubmit={Submit}>
         <div className='flex flex-col'> 
           <InputTags name={"username"} label={"Username:"} type={"text"} placeholder={"Username"}></InputTags>
           <InputTags name={"password"} label={"Password: "} type={"password"} placeholder={"Password"}></InputTags>
-        </div>
+        </div> 
+        <div className='h-6 mt-5 text-sm text-red-700'>{error}</div>
         <div className="flex">
         <button type="submit" className='w-[300px] h-[50px] bg-[#1e1c1e] mt-6 mx-auto  hover:bg-[#393639] transition-all duration-200 rounded-md'>SignUp</button>
         <a href="/auth/google" className='mx-auto mt-6 ms-2 bg-[#1e1c1e] w-[300px] h-[50px] text-center p-3 rounded-md hover:bg-[#393639] transition-all duration-200'>Login with Google</a>  
